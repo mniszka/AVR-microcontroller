ec7b1064725e81fe9cf40361cddeaeab79b4dbab
mniszka <aflisnik@wp.pl>
2021-07-20 17:19:34 +0200

    Update LED display.c

--- LED display.c
+++ LED display.c
@@ -1,42 +1,37 @@
-/*
- * main.c
- *
- *  Created on: 21 lut 2020
- *      Author: Aga
- */
-
-
 #include <avr/io.h>
-//#define F_CPU 8000000
+#define F_CPU 8000000
 #include <util/delay.h>
 int main()
 {
-int8_t i;
-unsigned char liczba_LED[11] = {0x3F, 0x6, 0x6, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x7, 0x7F, 0x6F};
+int8_t i, k, m;
+unsigned char liczba_LED[10] = {
+		  0x3F,	// 0
+			0x06,	// 1
+			0x5B,	// 2
+			0x4F,	// 3
+			0x66,	// 4
+			0x6D,	// 5
+			0x7D,	// 6
+			0x27,	// 7
+			0x7F,	// 8
+			0x6F,	// 9
+			};
 
-:
-DDRB |= (1<<PB0)| (1<<PB1) | (1<<PB2) | (1<<PB3) | (1<<PB4) | (1<<PB5) | (1<<PB6); //output
-DDRC |= (1<<PC0) | (1<<PC1); //output
-
-
-PORTC |= (1<<PC0) | (1<<PC1); //PC0 and PC1 is +
-
+//ustaw kierunki transmisji
+DDRB = 0xFF;
+DDRC = 0xFF;
 for(;;)
 {
-
-for(i=0; i<=10; i++) //digits 0 - 9
+for(i=0; i<=9; i++)
+for(k=0; k<=9; k++)
+for(m=1; m<=10; m++)
 {
+PORTC = 0x01;
+PORTB = liczba_LED[k];
+_delay_ms(5);
+PORTC = 0x02;
 PORTB = liczba_LED[i];
-//czekaj 1/4 sekundy
-_delay_ms(250);
-}
-
-for(i=10; i>=0; i--) //digits 9 - 0
-{
-PORTB = liczba_LED[i];
-
-//czekaj ¼ sekundy
-_delay_ms(250);
+_delay_ms(5);
 }
 }
 return 0;
